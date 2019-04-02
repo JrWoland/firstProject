28971570b2a58daaaeb22b8c3495f6f6de526d85
Marcin Kozicki <m.kozicki2@gmail.com>
2019-04-02 22:05:39 +0200

    New navbar

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-const buttonClick = document.querySelectorAll('figcaption');
+const buttons = document.querySelectorAll('figcaption');
 const div = document.querySelectorAll('.test');
 
-buttonClick.forEach((button, i) => {
+// sections collapse/expand script
+buttons.forEach((button, i) => {
 
     div[i].style.height = '0px';
 
@@ -19,15 +20,44 @@ buttonClick.forEach((button, i) => {
     })
 })
 
+// nav bar scripts
 const headerImage = document.querySelector('.mainHeader');
 const mainContainer = document.querySelector('main');
 const nav = document.querySelector('nav');
+const icon = document.querySelector('nav div i');
+const ul = document.querySelector('nav ul');
+const li = document.querySelectorAll('nav ul li');
 
-window.addEventListener('scroll', () => {
+const navAnimation = () => {
     if (scrollY < mainContainer.offsetTop - nav.offsetHeight) {
         headerImage.style.transform = `translateY(${-scrollY/3}px)`;
         nav.classList.remove('active');
     } else {
         nav.classList.add('active');
     }
+}
+let flag = true;
+icon.addEventListener('click', () => {
+
+    icon.classList.toggle('animate');
+    ul.classList.toggle('display');
+    nav.classList.toggle('display');
+    if (flag) {
+        const index = setTimeout(() => {
+            li.forEach((item) => {
+                item.style.opacity = 1;
+                item.style.marginTop = '5vh';
+                flag = !flag;
+            })
+            clearTimeout(index);
+        }, 100);
+
+    } else {
+        li.forEach((item) => {
+            item.style.opacity = 0;
+            item.style.marginTop = '0px';
+            flag = !flag;
+        })
+    }
 })
+window.addEventListener('scroll', navAnimation);
